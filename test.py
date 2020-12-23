9f7107b1b42649a05224c2b99c88e9f79fd90ef4
rom1ch <rdontsay@gmail.com>
2020-12-23 19:09:58 +0300

    Добавил заготовки

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 class FoodInfo:
-    pass
+    '''
+    Класс для удобного хранения калорийности продуктов.
+    '''
+    
+    def get_proteins(self):
+        pass
+
+    def get_fats(self):
+        pass
+
+    def get_carbohydrates(self):
+        pass
+    
+    def get_kcalories(self):
+        pass
